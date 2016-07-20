a2e66a9bd1c5ed6f4316eb1f24ab84687e7bbe7f
Kornelius Kalnbach <murphy@rubychan.de>
2016-07-20 22:11:56 +0200

    whitespace

--- tools/random.js
+++ tools/random.js
@@ -1,30 +1,33 @@
-var Random = (function() {
+var Random = (function () {
   // initialization is slow, cache this!
   var generator = new Alea(0);
+
   var random = {
-    init: function(seed) {
+    init: function (seed) {
       generator = new Alea(seed); // generator.setSeed(seed);
     },
-    nextFraction: function() {
+    nextFraction: function () {
       return generator();
     },
-    next: function(n) {
+    next: function (n) {
       if (n === undefined) n = 2;
       return Math.floor(this.nextFraction() * n);
     },
-    nextInRange: function(min, max) {
+    nextInRange: function (min, max) {
       return min + this.nextFraction() * (max - min);
     },
-    nextColor: function() {
+    nextColor: function () {
       return 'rgb(' +
         this.next(256) + ',' +
         this.next(256) + ',' +
         this.next(256) + ')';
     },
-    nextElement: function(array) {
+    nextElement: function (array) {
       return array[this.next(array.length)];
     }
   };
+
   random.init(0);
+
   return random;
 })();
